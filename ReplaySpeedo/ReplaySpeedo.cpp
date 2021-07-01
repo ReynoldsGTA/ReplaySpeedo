7532eb959d7dd6a35f196adef1d67dbc90f490b9
Gerben <gerbenhettinga@hotmail.com>
2021-07-02 01:01:50 +0200

    added asi file

--- ReplaySpeedo/ReplaySpeedo.cpp
+++ ReplaySpeedo/ReplaySpeedo.cpp
@@ -20,15 +20,14 @@ public:
 
     static unsigned m_currentFrame;
     static float m_speeds[MAX_FRAMES];
-    static float m_tryValue;
-    static int m_tryValue2;
+    static float m_blurSpeed;
 
     static void process() {
         //this will be processed once per frame
         m_player = FindPlayerPed();
         m_vehicle = FindPlayerVehicle(0, false);
 
-        m_tryValue = 0.0;
+        m_blurSpeed = 0.0;
 
         if (m_vehicle) {
             CFont::SetColor(CRGBA(200, 200, 200, 255));
@@ -49,7 +48,7 @@ public:
             avg /= float(MAX_FRAMES);
 
 
-            m_tryValue = fmin((avg *100.0) / 200.0, 1.0);
+            m_blurSpeed = fmin((avg *100.0) / 200.0, 1.0);
             //m_vehicle->m_fMovingSpeed = avg;
             //CVector s = m_vehicle->m_vecMoveSpeed;
             //m_vehicle->m_vecMoveSpeed = v;
@@ -66,8 +65,7 @@ public:
     }
 
     ReplaySpeedo() {
-        m_tryValue = 0.0;
-        m_tryValue2 = 1;
+        m_blurSpeed = 0.0;
         
         //replace original code with jmp to code cave below
         //jmp 00747C60
@@ -102,7 +100,7 @@ public:
         patch::SetChar(0x747C6C, 255);
         patch::SetChar(0x747C6D, 144);
         //217 5 224 81 141 0 216 26 233 137 180 251 255 144
-        patch::SetPointer(0x747C62, &m_tryValue);
+        patch::SetPointer(0x747C62, &m_blurSpeed);
 
 
         //add process function to the game so that it will run it every tick
@@ -118,8 +116,7 @@ CVehicle* ReplaySpeedo::m_vehicle;
 CVector ReplaySpeedo::m_oldPos;
 
 
-float ReplaySpeedo::m_tryValue;
-int ReplaySpeedo::m_tryValue2;
+float ReplaySpeedo::m_blurSpeed;
 
 unsigned ReplaySpeedo::m_currentFrame;
 float ReplaySpeedo::m_speeds[MAX_FRAMES];
